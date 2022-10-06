4c31223e346b751e8007bed7676064986b3fa83a
simonamiel <sim.47photmail.fr>
2022-10-06 16:00:37 +0200

    Project P7 step 3 add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import Logo from "../Images/logo.svg"
- 
+import HeaderCSS from "../Header/Header.module.css"
+
 function Header() {
-    return (
-        <nav>
-            <img src={Logo} alt="Logo de Kasa"/>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A Propos</Link>
+  return (
+      <header className={HeaderCSS.header}>
+        <img src={Logo} alt="Logo de Kasa" className={HeaderCSS.logo} />
+        <nav className={HeaderCSS.nav}>
+          <Link to="/" className={HeaderCSS.homeNav}>
+            Accueil
+          </Link>
+          <Link to="/about" className={HeaderCSS.aboutNav}>
+            A Propos
+          </Link>
         </nav>
-    )
+      </header>
+  )
 }
 
 export default Header
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
     return (
         <div>
-            <h1>Accueil</h1>
+            <h1></h1>
         </div>
     )
 }
