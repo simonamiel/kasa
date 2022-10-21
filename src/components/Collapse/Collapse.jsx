1c24b3de4a8c587399a7c446125ed41c81220307
simonamiel <sim.47photmail.fr>
2022-10-21 10:25:00 +0200

    Project 7 step 8 add lodgings page final add collapse responsive fourth part

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,24 +1,25 @@
 import CollapseCSS from "../Collapse/Collapse.module.css"
 import { useState } from "react"
+import { useParams } from "react-router-dom"
+import { DataLodgings } from "../../Data/DataLodgings"
 import iconCollapse from "../Images/icon-collapse.svg"
 
 function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false)
 
   return isOpen ? (
-    <div className={CollapseCSS.collapse}>
-      <div className={CollapseCSS.title} onClick={() => setIsOpen(false)} >
+    <div className={CollapseCSS.container}>
+      <div className={CollapseCSS.title} onClick={() => setIsOpen(false)}>
         {title}
-        <img src={iconCollapse} className={CollapseCSS.iconOpen}/>
+        <img src={iconCollapse} className={CollapseCSS.iconOpen} />
       </div>
       <div className={CollapseCSS.content}>{content}</div>
     </div>
-    
   ) : (
-    <div className={CollapseCSS.collapse}>
-        <div className={CollapseCSS.title} onClick={() => setIsOpen(true)}>
+    <div className={CollapseCSS.container}>
+      <div className={CollapseCSS.title} onClick={() => setIsOpen(true)}>
         {title}
-        <img src={iconCollapse} className={CollapseCSS.icon}/>
+        <img src={iconCollapse} className={CollapseCSS.icon} />
       </div>
     </div>
   )
--- src/pages/Lodgings/Lodgings.jsx
+++ src/pages/Lodgings/Lodgings.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React from "react"
+import { useParams } from "react-router-dom"
+import styled from 'styled-components'
+
 import Slideshow from "../../components/Slideshow/Slideshow"
+import LodgingsCSS from "../Lodgings/Lodgings.module.css"
 import LodgingDetails from "../../components/LodgingDetails/LodgingDetails"
+import Collapse from "../../components/Collapse/Collapse"
 /*import DataLodgings from Data folder*/
 import {DataLodgings} from "../../Data/DataLodgings"
-import { useParams } from "react-router-dom"
 
 export default function Lodgings() {
     let { id } = useParams ()
   const lodging = DataLodgings.getOneLodging(id)
+
     return (
         <div>
             <Slideshow key={lodging.pictures} images={lodging.pictures}/>
             <LodgingDetails key={lodging.title} details={lodging}/>
+            <div className={LodgingsCSS.collapse}>
+            <Collapse key={lodging.description} title="Description" content={lodging.description}/>
+            <Collapse key={lodging.equipments} title="Equipments" content={lodging.equipments.map((infos, index) => (
+                <div key={`${lodging.equipments}-${index}`}>{infos}</div>
+            ))}/>
+            </div>
         </div>
     )
 }
